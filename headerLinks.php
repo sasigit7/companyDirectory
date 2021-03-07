49b3bacaef35e1fcbee51d68b0c268571cad3a69
Shashi <sasiwebdev7@gmail.com>
2021-03-07 21:25:29 +0000

    Added header links

---
+++ headerLinks.php
@@ -0,0 +1,17 @@
+<head>
+    <title>Company Directory</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords"
+        content="It’s a rigorous project where the sign off of a user requirements document was required to trigger the release of SQL, allowing to develop a “mobile-first” application to maintain a company personnel database (PHP&MySQL). The additional layer of rigor is that the sign off is only achieved upon the trainee programmer supplying an independently witnessed document confirming the system’s ability to perform error-free." />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="Assets/css/styles.css" rel='stylesheet' type='text/css' media="all">
+    <link rel="icon" type="image/x-icon" href="favicon.ico?v=2" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+
+</head>
